f3111b02a5d6501d60024931b44736e9e624ccfd
George Curry <Georgemcurry1@gmail.com>
2025-03-03 17:15:00 -0600

    Update

--- Countdown.py
+++ Countdown.py
@@ -1,3 +1,11 @@
 #hello
-print("Hello User, do you want to set up a countdown or countup? Press 1 for countdown or 2 for countup")
+print("Hello User, do you want to set up a countdown or countup? Press 1 for countdown or 2 for countup ")
 
+choice = int(input())
+
+if choice == 1:
+    print("user chose 1")
+elif choice == 2:
+    print("user chose 2")
+else:
+    choice = input("Please input the correct value, 1 for countdown or 2 for count up ")
