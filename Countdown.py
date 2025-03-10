d0429318136a7899f19be7a86fe8d34ee7924106
George Curry <Georgemcurry1@gmail.com>
2025-03-10 16:56:20 -0500

    Finished.

--- Countdown.py
+++ Countdown.py
@@ -1,28 +1,33 @@
 #functions
 
 def countDown():
-    countDownStart = input("enter the number you want to count down from?")
-    for i in range(int(countDownStart),0 -1):
+    countDownStart = int(input("enter the number you want to count down from: "))
+    for i in range (countDownStart,0, -1):
         print(i)
 
 def countUp():
-    countUpStart = input("enter the number you want to count up from?")
-    for i in range(0,int(countUpStart), 1):
+    countUpStart = int(input("enter the number you want to count up to: "))
+    for i in range (0,countUpStart, 1):
         print(i)
 
 
 
 
 
-print("Hello User, do you want to set up a countdown or countup? Press 1 for countdown or 2 for countup ")
+print("Hello User, do you want to set up a countdown or countup? Press 1 for countdown or 2 for countup: ")
 
 choice = int(input())
 
-if choice == 1:
-    countDown()
-
-elif choice == 2:
-    countUp()
+if not choice == 1 or not choice == 2:
+    while choice == 1 or not choice == 2:
+        choice = int(input("Please input the correct value, 1 for countdown or 2 for count up: "))
+    if choice == 1:
+        countDown()
+    elif choice == 2:
+        countUp()
 else:
-    choice = int(input("Please input the correct value, 1 for countdown or 2 for count up "))
-
+    if choice == 1:
+        countDown()    
+    elif choice == 2:
+        countUp()
+        
